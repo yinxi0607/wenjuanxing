f446479cb2b9b5dbfaa5ce0d60dfac30b9b0ab5f
niuhongbin <niuhongbin111@163.com>
2023-09-11 17:37:24 +0800

    add ahook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 // import List2 from "./List2.tsx";
 // import UseCallbackDemo from "./UseCallbackDemo.tsx";
-import useTitle from "./hooks/useTitle.ts";
-import useGetInfo from "./hooks/useGetInfo.ts";
+// import useTitle from "./hooks/useTitle.ts";
+// import useGetInfo from "./hooks/useGetInfo.ts";
 // import useMouse from "./hooks/useMouse.ts";
 // import UseRefDemo from "./UseRefDemo.tsx";
 // import UseMemoDemo from "./UseMemoDemo.tsx";
+// import {useTitle} from "ahooks"
 
 function App() {
-    useTitle("App page")
-    const {loading,info} = useGetInfo()
+    // useTitle("App pag1e")
+    // const {loading,info} = useGetInfo()
     // const {x,y} = useMouse()
     return (
         <>
             <p>App Page</p>
-            <p>
-                {loading? "加载中。。": info}
-            </p>
+            {/*<p>*/}
+            {/*    {loading? "加载中。。": info}*/}
+            {/*</p>*/}
             {/*<p>App page {x},{y}</p>*/}
 
             {/*<List2/>*/}
