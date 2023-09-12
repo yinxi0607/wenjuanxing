a0cb511bfdf9bcb341b7a8876ee6abaa891460eb
niuhongbin <niuhongbin111@163.com>
2023-09-12 15:03:13 +0800

    add sass module

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import List from "./pages/List.tsx";
 
 function App() {
 
     return (
         <>
-            <p>App Page</p>
-
+            <h1 style={{background:'yellow'}}>问卷星FE</h1>
+            <List/>
         </>
 
     )
 }
 
 export default App
+
+
+// src/components 目录 -- 组件
+// src/pages 目录 -- 页面(React 组件)
+// 业务-页面（跳转，切换，大面积的），组件（零件）
+// src/utils 目录 -- 工具函数
---
+++ src/pages/List.tsx
@@ -0,0 +1,39 @@
+import {FC, useState} from 'react';
+import styles from './List.module.scss'
+import QuestionCard from "../components/QuestionCard.tsx";
+
+const rawQuestionList = [
+    {_id: 'q1', title: '问卷1', isPublished: true, isStar: false, answerCount: 5, createdAt: '9月12日 13:26'},
+    {_id: 'q2', title: '问卷2', isPublished: false, isStar: true, answerCount: 2, createdAt: '9月11日 12:26'},
+    {_id: 'q3', title: '问卷3', isPublished: false, isStar: false, answerCount: 0, createdAt: '9月18日 11:26'},
+    {_id: 'q4', title: '问卷4', isPublished: true, isStar: true, answerCount: 1, createdAt: '9月15日 19:26'}
+]
+
+const List: FC = () => {
+
+    const [questionList, setQuestionList] = useState(rawQuestionList)
+
+    return (
+        <>
+            <div className={styles.header}>
+                <div className={styles.left}>
+                    <h3>我的问卷</h3>
+                </div>
+                <div className={styles.right}>(搜索)</div>
+            </div>
+            <div>
+                {questionList.map(q=> {
+                    const { _id } = q
+                    return (
+                        <QuestionCard key={_id} {...q}/>
+                    )
+                })}
+            </div>
+            <div>
+                footer
+            </div>
+        </>
+    );
+};
+
+export default List;
