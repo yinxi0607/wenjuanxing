0bb81a2e2d49039a32a77164446afa98291b525c
niuhongbin <niuhongbin111@163.com>
2023-09-12 10:44:04 +0800

    init master

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-// import List2 from "./List2.tsx";
-import StyledComponentsDemo from "./StyledComponentsDemo.tsx";
-// import UseCallbackDemo from "./UseCallbackDemo.tsx";
-// import useTitle from "./hooks/useTitle.ts";
-// import useGetInfo from "./hooks/useGetInfo.ts";
-// import useMouse from "./hooks/useMouse.ts";
-// import UseRefDemo from "./UseRefDemo.tsx";
-// import UseMemoDemo from "./UseMemoDemo.tsx";
-// import {useTitle} from "ahooks"
-
-// import ClosureTrapDemo from "./ClosureTrapDemo.tsx";
 
 function App() {
-    // useTitle("App pag1e")
-    // const {loading,info} = useGetInfo()
-    // const {x,y} = useMouse()
+
     return (
         <>
             <p>App Page</p>
-            {/*<ClosureTrapDemo/>*/}
-            {/*<p>*/}
-            {/*    {loading? "加载中。。": info}*/}
-            {/*</p>*/}
-            {/*<p>App page {x},{y}</p>*/}
 
-            {/*<List2/>*/}
-            {/*<UseRefDemo/>*/}
-            {/*<UseMemoDemo/>*/}
-            {/*<UseCallbackDemo/>*/}
-            <StyledComponentsDemo/>
         </>
 
     )
