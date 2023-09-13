e46bbc86a3ec95f89780cdb013dbb8f3cfa2d38c
niuhongbin <niuhongbin111@163.com>
2023-09-14 07:51:51 +0800

    add mockjs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,24 @@
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 import {useNavigate} from "react-router-dom";
 import {Button, Typography} from "antd";
 import styles from './Home.module.scss'
 import {MANAGE_LIST_PATHNAME} from "../router";
+// import axios from "axios";
+// import '../_mock/index.ts'
+
 const {Title,Paragraph} = Typography
 const Home: FC = () => {
     const nav = useNavigate()
     // function clickHandler(){
     //     nav("/login")
     // }
+    //
+    // useEffect(()=>{
+    //     // fetch('/api/test').then(res=>res.json()).then(data=> console.log(data))
+    //     // mock.js 只能劫持 XMLHttpRequest，不能劫持fetch
+    //     axios.get('/api/test').then(res=> console.log(res.data))
+    // },[])
+
     return (
         <div className={styles.container}>
             <div className={styles.info}>
