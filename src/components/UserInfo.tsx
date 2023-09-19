bea76c27eda9fde5f53240b65c334fb65bc1c884
niuhongbin <niuhongbin111@163.com>
2023-09-19 10:46:47 +0800

    add userinfo mange

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,11 +1,37 @@
-import React, {FC} from 'react';
-import {Link} from "react-router-dom";
+import {FC} from 'react';
+import {Link, useNavigate} from "react-router-dom";
 import {LOGIN_PATHNAME} from "../router";
+import {useRequest} from "ahooks";
+import {getUserInfoService} from "../services/user.ts";
+import {UserOutlined} from "@ant-design/icons";
+import {Button, message} from "antd";
+import {removeToken} from "../utils/user-token.ts";
 
 const UserInfo: FC = () => {
+    const nav = useNavigate()
+    const {data} = useRequest(getUserInfoService)
+    console.log("userinfo data",data)
+    const {username,nickname} = data || {}
+    function logout(){
+        removeToken()// 清楚token的存储
+        message.success("退出成功")
+        nav(LOGIN_PATHNAME)
+    }
+    const UserInfo = (
+        <>
+            <span style={{color:'#e8e8e8'}}>
+                <UserOutlined/>
+                {nickname || username}
+            </span>
+            <Button type="link" onClick={logout}>退出</Button>
+        </>
+    )
+    const Login= (
+        <Link to={LOGIN_PATHNAME}>登录</Link>
+    )
     return (
         <>
-            <Link to={LOGIN_PATHNAME}>登录</Link>
+            {username?UserInfo:Login}
         </>
     );
 };
